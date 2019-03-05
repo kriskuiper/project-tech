e10319dc8d182c8e27839e9a1a0fb560a463e2ea
kriskuiper <kriskuiper74@gmail.com>
2019-03-05 11:52:51 +0100

    Let server render dynamic data without template engine, setup views and require ejs

--- index.js
+++ index.js
@@ -25,27 +25,64 @@ const postsdata = [
     }
 ];
 
+// All server thingies
 app
     // serve static files that are in the static directory
     .use('/static', express.static('static'))
-    .get('/', onhome)
-    .listen(8000)
 
-function onhome(req, res) {
-    let doc = '<!doctype.html>';
+    // set ejs as templating engine
+    .set('view engine', 'ejs')
+    .set('views', 'view')
+    .get('/', home)
+    .get('/about', about)
+    .use(notFound)
+    .listen(8000);
+
+function home(req, res) {
+    let doc = '<!doctype html>';
     let length = postsdata.length;
     let index = -1;
     let routepost;
 
+    doc += '<link rel="stylesheet" href="static/index.css">';
+    doc += '<title>Home - CycLove</title>';
     doc += '<h1>Hello World</h1>';
     doc += '<p>Welcome to my dating app</p>';
 
+    // As long as you have posts, do this (inside the wile loop) 
+    // with every post
     while (++index < length) {
         routepost = postsdata[index];
 
+        // Title is post title
         doc += `<h2>${routepost.title}</h2>`;
+        // Content is post content
         doc += `<p>${routepost.contents}</p>`;
     }
+
+    // Respond with whole document
+    res.send(doc);
+}
+
+function about(req, res) {
+    let doc = '<!doctype html>';
+    
+    doc += '<link rel="stylesheet" href="static/index.css">';
+    doc += '<title>About us - CycLove</title>'
+    doc += '<h1>About us</h1>';
+    doc += '<p>CycLove is a dating app</p>';
+
+    res.send(doc);
+}
+
+// Send this 404 page when navigated to unknown page
+function notFound(req, res) {
+    let doc = '<!doctype html>';
+
+    doc += '<link rel="stylesheet" href="static/index.css">';
+    doc += '<title>404 Not found - CycLove</title>';
+    doc += '<h1>Used some drug?</h1>';
+    doc += '<p>You have navigated to a page we could not find</p>';
 
     res.send(doc);
 }
