9e2a47866ffc6a1bca5f2be53fd2c41d257b2758
kriskuiper <kriskuiper74@gmail.com>
2019-03-08 14:03:28 +0100

    Add ESLint, let server send static files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Require dependencies from node_modules
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 // Couple express to app variable
 const app = express();
@@ -27,33 +27,40 @@ const postsdata = [
         contents: "Hopped on my bike this afternoon for a ride to Amerongen",
         kms: 25,
         bike: "Cervélo 45J",
-    }
+        location: "Amsterdam"
+    },
 ];
 
 // All server thingies
 app
     // serve static files that are in the static directory
-    .use('/static', express.static('static'))
+    .use("/static", express.static("static"))
 
     // set ejs as templating engine
-    .set('view engine', 'ejs')
-    .set('views', 'view')
-    .get('/', home)
-    .get('/about', about)
+    .set("view engine", "ejs")
+    .set("views", "view")
+    .get("/", home)
+    .get("/my-feed", feed)
+
+    // If you can't find any of the gets defined above, render 404.
     .use(notFound)
+
+    // Listen at port
     .listen(port, () => {
         console.log(`Listening on port ${port}...`);
     });
 
 function home(req, res) {
-    res.render('partials/feed.ejs', {postsdata: postsdata});
+    // Have to render the static/index.html here... how?
+    res.sendFile(path.join(__dirname, "static/index.html"));
 }
 
-function about(req, res) {
-    res.render('partials/about.ejs');
+function feed(req, res) {
+    res.render("feed.ejs", {postsdata: postsdata});
 }
 
 // Send this 404 page when navigated to unknown page
 function notFound(req, res) {
-    res.render('partials/not-found.ejs');
+    // Have to render the static/404.html here... how?
+    res.sendFile(path.join(__dirname, "static/404.html"));
 }
