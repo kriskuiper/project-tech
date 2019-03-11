f44b5474fdc6e8c9edd75ceb9c01896403a9db25
kriskuiper <kriskuiper74@gmail.com>
2019-03-11 16:24:01 +0100

    Set up adding posts and post detail page

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Require dependencies from node_modules
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const find = require("array-find");
+const slug = require("slug");
 
 // Couple express to app variable
 const app = express();
@@ -35,12 +38,17 @@ const postsdata = [
 app
     // serve static files that are in the static directory
     .use("/static", express.static("static"))
+    
+    .use(bodyParser.urlencoded({extended: true}))
 
     // set ejs as templating engine
     .set("view engine", "ejs")
     .set("views", "view")
     .get("/", home)
     .get("/my-feed", feed)
+    .get("/:id", detail)
+    .post("/my-feed", addPost)
+    .get("/add-post", form)
 
     // If you can't find any of the gets defined above, render 404.
     .use(notFound)
@@ -50,6 +58,7 @@ app
         console.log(`Listening on port ${port}...`);
     });
 
+// Routes
 function home(req, res) {
     // Have to render the static/index.html here... how?
     res.sendFile(path.join(__dirname, "static/index.html"));
@@ -59,8 +68,32 @@ function feed(req, res) {
     res.render("feed.ejs", {postsdata: postsdata});
 }
 
+function form(req, res) {
+    res.render("add-post.ejs");
+}
+
+function detail(req, res) {
+    res.render("detail.ejs");
+}
+
+function addPost(req, res) {
+    const id = slug(req.body.title).toLowerCase();
+
+    postsdata.push({
+        id: id,
+        title: req.body.title,
+        author: req.body.author,
+        contents: req.body.contents,
+        kms: req.body.contents,
+        bike: req.body.bike,
+        location: req.body.location
+    });
+
+    res.redirect(`/${id}`);
+}
+
 // Send this 404 page when navigated to unknown page
 function notFound(req, res) {
     // Have to render the static/404.html here... how?
-    res.sendFile(path.join(__dirname, "static/404.html"));
+    res.status(404).sendFile(path.join(__dirname, "static/404.html"));
 }
