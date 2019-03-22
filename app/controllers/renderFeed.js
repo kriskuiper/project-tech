2d751f8b2d9a596d50611db79790b3d4b7264509
kriskuiper <kriskuiper74@gmail.com>
2019-03-22 09:01:28 +0100

    Write renderFeed async instead of promise chain

--- app/controllers/renderFeed.js
+++ app/controllers/renderFeed.js
@@ -1,22 +1,32 @@
 const FeedPost = require("../models/FeedPost");
 const requestUsers = require("../models/apidata");
 
-function renderFeed(req, res) {
-    requestUsers()
-        .then(renderUsers);
+// function renderFeed(req, res) {
+//     requestUsers()
+//         .then(renderUsers);
 
-    function renderUsers(users) {
-        FeedPost.find()
-            .then(usePosts)
-            .catch(logError);
+//     function renderUsers(users) {
+//         FeedPost.find()
+//             .then(usePosts)
+//             .catch(logError);
 
-        function usePosts(posts) {
-            res.render("feed.ejs", {
-                postsdata: posts,
-                users: users
-            });
-        }
-    }    
+//         function usePosts(posts) {
+//             res.render("feed.ejs", {
+//                 postsdata: posts,
+//                 users: users
+//             });
+//         }
+//     }    
+// }
+
+async function renderFeed(req, res) {
+    const posts = await FeedPost.find();
+    const users = await requestUsers();
+    
+    res.render("feed", {
+        posts: posts,
+        users: users
+    });
 }
 
 function logError(err) {
--- app/models/FeedPost.js
+++ app/models/FeedPost.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-
 const ObjectId = mongoose.ObjectId;
 const Schema = mongoose.Schema;
 
