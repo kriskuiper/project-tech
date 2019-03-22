391d334ce726d3e4f81f8d6da35cbc2d6600bbcc
kriskuiper <kriskuiper74@gmail.com>
2019-03-22 09:55:46 +0100

    Rewrite renderFeed async, it's awesomegit add .git add .

--- app/controllers/renderFeed.js
+++ app/controllers/renderFeed.js
@@ -1,32 +1,19 @@
 const FeedPost = require("../models/FeedPost");
 const requestUsers = require("../models/apidata");
 
-// function renderFeed(req, res) {
-//     requestUsers()
-//         .then(renderUsers);
-
-//     function renderUsers(users) {
-//         FeedPost.find()
-//             .then(usePosts)
-//             .catch(logError);
-
-//         function usePosts(posts) {
-//             res.render("feed.ejs", {
-//                 postsdata: posts,
-//                 users: users
-//             });
-//         }
-//     }    
-// }
-
-async function renderFeed(req, res) {
-    const posts = await FeedPost.find();
-    const users = await requestUsers();
-    
-    res.render("feed", {
-        posts: posts,
-        users: users
-    });
+async function renderFeed(req, res, next) {
+    try {
+        const posts = await FeedPost.find();
+        const users = await requestUsers();
+        const reversed = await posts.reverse();
+        
+        res.render("feed", {
+            posts: await reversed,
+            users: await users
+        });
+    } catch(error) {
+        next(error);
+    }
 }
 
 function logError(err) {
