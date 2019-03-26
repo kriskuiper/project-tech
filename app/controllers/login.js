c2104cac064190c27fea0bba9b17afeb4226dbe1
kriskuiper <kriskuiper74@gmail.com>
2019-03-26 19:49:22 +0100

    Set more back

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,7 +1,7 @@
 const User = require("../models/User");
 
 async function login(req, res, next) {
-    console.log(User.findOne({ username: req.body.username}));
+    const user = User.findOne({ username: req.body.username});
 }
 
 module.exports = login;
