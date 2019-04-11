82624cbe78905ab8a93098bb8e685425e776578f
Kris <kriskuiper74@gmail.com>
2019-04-12 01:32:49 +0200

    Add polyfill for intersectionobserver

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -1,29 +1,42 @@
+// DOM Elements
+const feedPosts = document.querySelectorAll(".feed__post");
+
 // Cutting the mustard technique
 if ("querySelector" in document) {
     document.body.classList.add("js-enabled");
-    document.cookie = "js_enabled=true";
+
+    const now = new Date();
+    now.setTime(now.getTime() + 1 * 3600 * 75);
+    document.cookie = `js_enabled=true; expires=${now.toUTCString()}; path=/`;
 }
 
 // Handle feed posts
-const feedPosts = document.querySelectorAll(".feed__post");
-const postObserver = new IntersectionObserver(showPosts);
+if ("IntersectionObserver" in window) {
+    const postObserver = new IntersectionObserver(showPosts);
 
-// Observe the feed posts
-feedPosts.forEach(observeFeedPost);
+    // Observe the feed posts
+    feedPosts.forEach(observeFeedPost);
 
-function observeFeedPost(feedPost) {
-  postObserver.observe(feedPost);
-}
-
-// Callback for the intersection observer, add the is--visible class to an entry if it is intersecting with the IU
-function showPosts(entries) {
-  entries.forEach(showPost);
-
-  function showPost(entry) {
-    const entryClass = entry.target.classList;
-
-    if (entry.isIntersecting) {
-      entryClass.add("is--visible");
+    function observeFeedPost(feedPost) {
+      postObserver.observe(feedPost);
     }
-  }
+
+    // Callback for the intersection observer, add the is--visible class to an entry if it is intersecting with the IU
+    function showPosts(entries) {
+      entries.forEach(showPost);
+
+      function showPost(entry) {
+        const entryClass = entry.target.classList;
+
+        if (entry.isIntersecting) {
+          entryClass.add("is--visible");
+        }
+      }
+    }
+} else {
+    feedPosts.forEach(feedPost, makeVisible);
+
+    function makeVisible(feedPost) {
+      feedPost.classList.add("is--visible");
+    }
 }
